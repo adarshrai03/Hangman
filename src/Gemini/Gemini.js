318671eafed498a9d49616111f786896683025d0
adarshrai03 <147544218+adarshrai03@users.noreply.github.com>
2025-04-29 00:38:47 +0530

    Update Gemini.js

--- src/Gemini/Gemini.js
+++ src/Gemini/Gemini.js
@@ -10,7 +10,7 @@ import {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.0-pro",
+    model: "gemini-2.0-flash",
   });
   
   const generationConfig = {
